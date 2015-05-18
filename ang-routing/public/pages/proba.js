3c96fd02f25b490cb574ad90524e85e95119e2b7
Danilo Zekovic <dzv9776@saintjoe.edu>
2015-05-18 11:18:01 -0400

    basic html stuff that is added when on proba

---
+++ ang-routing/public/pages/proba.js
@@ -0,0 +1,7 @@
+
+             '<div class="foo jumbotron text-center">
+               <h1>Proba Page</h1>
+               <p>{{ message }}</p>
+               <button>Submit</button>
+               <div>{{doSomething}}</div>
+             </div>'
